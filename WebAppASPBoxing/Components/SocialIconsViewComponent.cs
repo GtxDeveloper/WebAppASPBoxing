5e156bfd36ce8e1eddeba8ece73585f3541a6455
BanderolyaDeloveper <131999206+BanderolyaDeloveper@users.noreply.github.com>
2024-06-06 16:38:00 +0300

    Добавьте файлы проекта.

---
+++ WebAppASPBoxing/Components/SocialIconsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppASPBoxing.Components
+{
+    public class SocialIconsViewComponent:ViewComponent
+    {
+        private List<KeyValuePair<string, string>> _socialIcons;
+
+        public SocialIconsViewComponent() 
+        {
+            _socialIcons = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("https://uk-ua.facebook.com/","images/facebook.png"),
+                new KeyValuePair<string, string>("https://x.com/?lang=ru","images/twitter.png"),
+                new KeyValuePair<string, string>("https://ua.linkedin.com/","images/linkedin.png"),
+                new KeyValuePair<string, string>("https://www.instagram.com/","images/instagram.png"),
+                new KeyValuePair<string, string>("https://www.youtube.com/","images/youtube.png")
+            };
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("SocialIcons", _socialIcons);
+        }
+    }
+}
---
+++ WebAppASPBoxing/Models/ClientMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppASPBoxing.Models
+{
+    public class ClientMessage
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, заполните Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, заполните Phone")]
+        public string Phone { get; set; }
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Пожалуйста, заполните валидный Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, заполните Message")]
+        public string Message { get; set; }
+    }
+}
